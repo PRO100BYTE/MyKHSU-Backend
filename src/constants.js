6ef0368dfa5411f1dc377ef4285bddffc0196497
Evgeniy Struchkov <thedayg0ne@internet.ru>
2026-04-23 13:41:53 +0700

    Update constants.js
    Co-Authored-By: PRO100BYTE Helper <helper@pro100byte.ru>
    Signed-off-by: Evgeniy Struchkov <thedayg0ne@internet.ru>

--- src/constants.js
+++ src/constants.js
@@ -1,18 +1,18 @@
 export const APP_CONSTANTS = {
-  appName: 'MyKHSU Backend',
-  appShortName: 'MyKHSU',
-  apiVersion: '1.1.0',
-  appVersion: '1.1.0',
+  appName: 'Мой ИТИ ХГУ Backend (NodeJS Port by TheDayG0ne)',
+  appShortName: 'MyKHSUBackend',
+  apiVersion: '1.2.5',
+  appVersion: '1.5.3',
   buildNumber: '2026.04.22.1',
   buildDate: '2026-04-22T00:00:00.000Z',
   adminPanelVersion: '1.1.0',
   frontendVersion: '1.1.0',
   contacts: {
-    team: 'ITI KHSU',
+    team: 'PRO100BYTE Team',
     projectUrl: 'https://github.com/PRO100BYTE/MyKHSU',
   },
 };
 
 export const ADMIN_DEFAULTS = {
-  usernames: ['admin', 'TheDayG0ne'],
+  usernames: ['admin', 'superadmin'],
 };
